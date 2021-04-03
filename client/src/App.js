571e1140e4780127cf4365a09c417c470e8b859b
PD606 <praveenh.cs17@sahyadri.edu.in>
2021-04-03 22:55:48 +0530

    completed fileupload and return hash

--- client/src/App.js
+++ client/src/App.js
@@ -1,71 +1,144 @@
 import React, { Component } from "react";
 import SimpleStorageContract from "./contracts/SimpleStorage.json";
 import getWeb3 from "./getWeb3";
-
+import ipfs from "./ipfs";
 import "./App.css";
 
 class App extends Component {
-  state = { storageValue: 0, web3: null, accounts: null, contract: null };
+  state = { ipfsHash: 0, web3: null, accounts: null, contract: null };
 
-  componentDidMount = async () => {
-    try {
-      // Get network provider and web3 instance.
-      const web3 = await getWeb3();
+  constructor(){
+    super();
+    this.captureFile = this.captureFile.bind(this);
+    this.onSubmit=this.onSubmit.bind(this);
 
-      // Use web3 to get the user's accounts.
-      const accounts = await web3.eth.getAccounts();
+  }
 
-      // Get the contract instance.
-      const networkId = await web3.eth.net.getId();
-      const deployedNetwork = SimpleStorageContract.networks[networkId];
-      const instance = new web3.eth.Contract(
-        SimpleStorageContract.abi,
-        deployedNetwork && deployedNetwork.address,
-      );
+  // componentDidMount = async () => {
+  //   try {
+  //     // Get network provider and web3 instance.
+  //     const web3 = await getWeb3();
 
-      // Set web3, accounts, and contract to the state, and then proceed with an
-      // example of interacting with the contract's methods.
-      this.setState({ web3, accounts, contract: instance }, this.runExample);
-    } catch (error) {
-      // Catch any errors for any of the above operations.
-      alert(
-        `Failed to load web3, accounts, or contract. Check console for details.`,
-      );
-      console.error(error);
+  //     // Use web3 to get the user's accounts.
+  //     const accounts = await web3.eth.getAccounts();
+
+  //     // Get the contract instance.
+  //     const networkId = await web3.eth.net.getId();
+  //     const deployedNetwork = SimpleStorageContract.networks[networkId];
+  //     const instance = new web3.eth.Contract(
+  //       SimpleStorageContract.abi,
+  //       deployedNetwork && deployedNetwork.address,
+  //     );
+
+  //     // Set web3, accounts, and contract to the state, and then proceed with an
+  //     // example of interacting with the contract's methods.
+  //     this.setState({ web3, accounts, contract: instance }, this.runExample);
+  //   } catch (error) {
+  //     // Catch any errors for any of the above operations.
+  //     alert(
+  //       `Failed to load web3, accounts, or contract. Check console for details.`,
+  //     );
+  //     console.error(error);
+  //   }
+  // };
+
+  // runExample = async () => {
+  //   const { accounts, contract } = this.state;
+
+  //   // Stores a given value, 5 by default.
+  //   // await contract.methods.set(75).send({ from: accounts[0] });
+
+  //   // Get the value from the contract to prove it worked.
+
+  //   console.log('hererere');
+  //   const response = await contract.methods.get().call(accounts[0]);
+
+  //   // Update state with the result.
+  //   this.setState({ ipfsHash: response });
+  // };
+
+  captureFile(event){
+
+    console.log('capture');
+    event.preventDefault();
+    const file = event.target.files[0];
+    console.log(file);
+
+    const reader = new window.FileReader();     // convert to array which buufer understands
+    reader.readAsArrayBuffer(file);
+
+    console.log(reader);
+    reader.onloadend = () =>{
+      this.setState({buffer: Buffer(reader.result)})
+
+      console.log(this.state.buffer);
+
     }
-  };
 
-  runExample = async () => {
-    const { accounts, contract } = this.state;
+    
 
-    // Stores a given value, 5 by default.
-    await contract.methods.set(75).send({ from: accounts[0] });
+  }
 
-    // Get the value from the contract to prove it worked.
-    const response = await contract.methods.get().call();
+  onSubmit(event){
 
-    // Update state with the result.
-    this.setState({ storageValue: response });
-  };
+
+    event.preventDefault();
+    console.log('submit');
+
+    ipfs.files.add(this.state.buffer, (error,result) =>{
+      if(error){
+        console.log(error);
+        return
+      }
+
+      const { accounts, contract } = this.state;
+
+      // console.log(result[0].hash); 
+      var content = document.getElementById("content");
+      var hashValue = document.getElementById("hashValue");
+
+      content.innerText="File Uploaded successfully";
+      var a = document.createElement('a');
+
+      a.href=`https://ipfs.io/ipfs/${result[0].hash}`;
+      
+      hashValue.innerHTML = a
+
+      // contract.methods.set(result[0].hash).send({ from: accounts[0]}).then(
+        console.log('newwwwwwwww',this.ipfsHash)
+
+    
+
+      return this.setState({
+        ipfsHash: result[0].hash
+        
+      })
+    })
+    // console.log('yoooo',this.state.ipfsHash);
+
+  }
 
   render() {
-    if (!this.state.web3) {
-      return <div>Loading Web3, accounts, and contract...</div>;
-    }
+    // if (!this.state.web3) {
+    //   return <div>Loading Web3, accounts, and contract...</div>;
+    // }
     return (
       <div className="App">
         <h1>Good to Go!</h1>
-        <p>Your Truffle Box is installed and ready.</p>
-        <h2>Smart Contract Example</h2>
-        <p>
-          If your contracts compiled and migrated successfully, below will show
-          a stored value of 5 (by default).
-        </p>
-        <p>
-          Try changing the value stored on <strong>line 40</strong> of App.js.
-        </p>
-        <div>The stored value is: {this.state.storageValue}</div>
-      </div>
+        <p id='content'>Your Truffle Box is installed and ready.</p>
+        <p id='hashValue'> Here</p>
+        <h2>Upload Files to IPFS</h2>
+
+
+        <form onSubmit= {this.onSubmit}>
+
+            <input type = "file" onChange = {this.captureFile}/>
+            <input type = "submit"/>
+
+      
+        </form>
+
+        </div>
     );
   }
 }
--- contracts/SimpleStorage.sol
+++ contracts/SimpleStorage.sol
@@ -1,14 +1,14 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >=0.4.21 <0.7.0;
+pragma solidity >=0.4.17 <=0.8.3;
 
 contract SimpleStorage {
-  uint storedData;
+  string storedData;
 
-  function set(uint x) public {
+  function set(string memory x) public {
     storedData = x;
   }
 
-  function get() public view returns (uint) {
+  function get() public view returns (string memory) {
     return storedData;
   }
 }
