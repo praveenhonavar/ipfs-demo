14f48b299ad4bc34a4bcbf4480909725138b7635
PD606 <praveenh.cs17@sahyadri.edu.in>
2021-04-04 23:30:47 +0530

    added to bc

--- client/src/App.js
+++ client/src/App.js
@@ -14,84 +14,83 @@ class App extends Component {
 
   }
 
-  // componentDidMount = async () => {
-  //   try {
-  //     // Get network provider and web3 instance.
-  //     const web3 = await getWeb3();
+  componentDidMount = async () => {
+    
+    try {
 
-  //     // Use web3 to get the user's accounts.
-  //     const accounts = await web3.eth.getAccounts();
+      console.log('innnnnuuuu');
+      // Get network provider and web3 instance.
+      const web3 = await getWeb3();
 
-  //     // Get the contract instance.
-  //     const networkId = await web3.eth.net.getId();
-  //     const deployedNetwork = SimpleStorageContract.networks[networkId];
-  //     const instance = new web3.eth.Contract(
-  //       SimpleStorageContract.abi,
-  //       deployedNetwork && deployedNetwork.address,
-  //     );
+      // Use web3 to get the user's accounts.
+      const accounts = await web3.eth.getAccounts();
 
-  //     // Set web3, accounts, and contract to the state, and then proceed with an
-  //     // example of interacting with the contract's methods.
-  //     this.setState({ web3, accounts, contract: instance }, this.runExample);
-  //   } catch (error) {
-  //     // Catch any errors for any of the above operations.
-  //     alert(
-  //       `Failed to load web3, accounts, or contract. Check console for details.`,
-  //     );
-  //     console.error(error);
-  //   }
-  // };
+      // Get the contract instance.
 
-  // runExample = async () => {
-  //   const { accounts, contract } = this.state;
+      const networkId = await web3.eth.net.getId();
+      const deployedNetwork = SimpleStorageContract.networks[networkId];
 
-  //   // Stores a given value, 5 by default.
-  //   // await contract.methods.set(75).send({ from: accounts[0] });
+      const instance = new web3.eth.Contract(
+        SimpleStorageContract.abi,
+        deployedNetwork && deployedNetwork.address,
 
-  //   // Get the value from the contract to prove it worked.
+      );
 
-  //   console.log('hererere');
-  //   const response = await contract.methods.get().call(accounts[0]);
+      // Set web3, accounts, and contract to the state, and then proceed with an
+      // example of interacting with the contract's methods.
+      this.setState({ web3, accounts, contract: instance }, this.runExample);
+    } catch (error) {
+      // Catch any errors for any of the above operations.
+      alert(
+        `Failed to load web3, accounts, or contract. Check console for details.`,
+      );
+      console.error(error);
+    }
+  };
 
-  //   // Update state with the result.
-  //   this.setState({ ipfsHash: response });
-  // };
+  runExample = async () => {
 
-  captureFile(event){
+    const { accounts, contract } = this.state;
+    // Stores a given value, 5 by default.
+    // await contract.methods.set("pd").send({ from: accounts[0] });
+    // Get the value from the contract to prove it worked.
+    console.log('hererere');
+    // const response = await contract.methods.get().call(accounts[0]);
+    // 
+    // Update state with the result.
+    // this.setState({ ipfsHash: response });
+  };
+
+  captureFile(event)
+  {
 
     console.log('capture');
     event.preventDefault();
     const file = event.target.files[0];
     console.log(file);
-
     const reader = new window.FileReader();     // convert to array which buufer understands
     reader.readAsArrayBuffer(file);
 
     console.log(reader);
     reader.onloadend = () =>{
-      this.setState({buffer: Buffer(reader.result)})
-
-      console.log(this.state.buffer);
+    this.setState({buffer: Buffer(reader.result)})
+    console.log(this.state.buffer);
 
     }
-
-    
-
   }
 
-  onSubmit(event){
+    onSubmit(event)
 
-
+    {
     event.preventDefault();
     console.log('submit');
 
     ipfs.files.add(this.state.buffer, (error,result) =>{
+      
       if(error){
         console.log(error);
         return
       }
-
-      const { accounts, contract } = this.state;
 
       // console.log(result[0].hash); 
       var content = document.getElementById("content");
@@ -101,13 +100,30 @@ class App extends Component {
       var a = document.createElement('a');
 
       a.href=`https://ipfs.io/ipfs/${result[0].hash}`;
+
+      // <img src='https://ipfs.io/ipfs/${result[0].hash}'/>
+
       
       hashValue.innerHTML = a
 
       // contract.methods.set(result[0].hash).send({ from: accounts[0]}).then(
-        console.log('newwwwwwwww',this.ipfsHash)
+      console.log(result[0].hash);
 
-    
+      const { accounts, contract } = this.state;
+
+      console.log('ccccccc',contract);
+
+      // await contract.methods.set(75).send({ from: accounts[0] });
+
+      contract.methods.set(result[0].hash).send({from :accounts[0]});
+
+      // const response =  contract.methods.get().call(accounts[0]);
+
+    // Update state with the result.
+
+      // this.setState({ ipfsHash: response });
+
+      // console.log('got from eth',ipfsHash);
 
       return this.setState({
         ipfsHash: result[0].hash
@@ -119,23 +135,27 @@ class App extends Component {
   }
 
   render() {
-    // if (!this.state.web3) {
-    //   return <div>Loading Web3, accounts, and contract...</div>;
-    // }
-    return (
-      <div className="App">
-        <h1>Good to Go!</h1>
-        <p id='content'>Your Truffle Box is installed and ready.</p>
-        <p id='hashValue'> Here</p>
-        <h2>Upload Files to IPFS</h2>
+    if (!this.state.web3) {
+      return <div>Loading Web3, accounts, and contract...</div>;
+    }
 
+    return (
+
+      <div className="App">
+
+        <h1>Good to Go!</h1>
+
+        <p id='content'>Your Truffle Box is installed and ready.</p>
+
+        <p id='hashValue'> Here</p>
+
+        <h2>Upload Files to IPFS</h2>
 
         <form onSubmit= {this.onSubmit}>
 
             <input type = "file" onChange = {this.captureFile}/>
             <input type = "submit"/>
-
-      
+            
         </form>
 
         </div>
